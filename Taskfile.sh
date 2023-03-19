7977165a10126b747164ab690381162abdc24555
Mo Blaa <11476187+MoBlaa@users.noreply.github.com>
2023-03-19 09:35:15 +0100

    added spotless

--- Taskfile.sh
+++ Taskfile.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
+mvn=./mvnw
+
 # Import .env file as environment variables
 [ -f .env ] && export $(grep -v '^#' .env | xargs -d '\n')
 
 function help {
-    echo "$0 <task> <args>"
-    echo "Tasks:"
-    compgen -A function | cat -n
+  echo "$0 <task> <args>"
+  echo "Tasks:"
+  compgen -A function | cat -n
 }
 
 function load_git_env {
-    echo export GIT_COMMITTER_NAME="$(git config user.name)"
-    echo export GIT_COMMITTER_EMAIL="$(git config user.email)"
-    echo export GIT_AUTHOR_NAME="$(git config user.name)"
-    echo export GIT_AUTHOR_EMAIL="$(git config user.email)"
+  echo export GIT_COMMITTER_NAME="$(git config user.name)"
+  echo export GIT_COMMITTER_EMAIL="$(git config user.email)"
+  echo export GIT_AUTHOR_NAME="$(git config user.name)"
+  echo export GIT_AUTHOR_EMAIL="$(git config user.email)"
+}
+
+function check {
+  $mvn spotless:check
+}
+
+function format {
+  $mvn spotless:apply
 }
 
 TIMEFORMAT="Task ${1:-help} completed in %3lR"
